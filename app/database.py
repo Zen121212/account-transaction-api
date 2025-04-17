787a787fb87434ec4bb6ad541b0e91718cf0c053
Zein Kassem <zein.s.kassem@gmail.com>
2025-04-17 20:30:51 +0300

    Make SQLALCHEMY_DATABASE_URL configurable via environment variable

--- app/database.py
+++ app/database.py
@@ -1,16 +1,21 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysecretpassword@postgres_db:5432/accounts_transactions"
-
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:mysecretpassword@postgres_db:5432/accounts_transactions"
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL", "postgresql://postgres:mysecretpassword@postgres_db:5432/accounts_transactions"
+)
 # Database setup
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 # Dependency to get a database session
+
+
 def get_db():
     db = SessionLocal()
     try:
